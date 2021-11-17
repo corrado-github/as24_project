713e6b6ad7ad8c1c02d2d97b85284c0952ea6e15
corradoboeche <corradoboeche@libero.it>
2021-11-17 12:35:46 +0100

    fixed two bugs: one in the time to wait to upload the page, one for empty rows

--- webpage2df.py
+++ webpage2df.py
@@ -36,9 +36,13 @@ def get_info_auto(list_autos, df_as24):
 
     for item in list_autos:
         n_row_df = len(df_as24)
-    #    pdb.set_trace()
         model_row = item.find_all('h2',{'class':"cldt-summary-makemodel sc-font-bold sc-ellipsis"})
-        model = model_row[0].contents[0]
+        try:
+            model = model_row[0].contents[0]
+        except:
+            print(model_row, model)
+            #if it does not find the model, then skip
+            continue
         df_as24.loc[n_row_df,'model'] = model
         #
         version_row = item.find_all('h2',{'class':"cldt-summary-version sc-ellipsis"})
@@ -65,25 +69,26 @@ def get_info_auto(list_autos, df_as24):
             for i, li_item in enumerate(list_li[0:len(list_features)]):
                 if list_features[i] == 'mileage':
                     value = li_item.contents[0].strip().split(' ')[0].replace('.','')
+                    print(n_row_df, list_features[i],check_is_number(value))
                     df_as24.loc[n_row_df, list_features[i]] = check_is_number(value)                    
                 elif list_features[i] == 'n_owners':
                     value = li_item.contents[0].strip().split()[0]
-                    df_as24.loc[n_row_df, list_features[i]] = check_is_number(value)
-    
+                    print(n_row_df, list_features[i],check_is_number(value))
+                    df_as24.loc[n_row_df, list_features[i]] = check_is_number(value)    
                 else:
+                    print(n_row_df, list_features[i],li_item.contents[0].strip())
                     df_as24.loc[n_row_df, list_features[i]] = li_item.contents[0].strip()
     return df_as24
 ##################
 #list of autos to scrap
 list_marca = ['Fiat', 'BMW', 'Renault','Citroen','Peugeot']
-list_modello = ['Punto Evo', 'i3', 'ZOE','C_Zero','iOn']
+list_modello = ['Punto Evo', 'i3', 'ZOE','C-Zero','iOn']
 list_file_out_name = ['data/AS24_Punto_Evo.csv', 'data/AS24_BMWi3.csv','data/AS24_ZOE.csv','data/AS24_CZero.csv','data/AS24_iOn.csv']
 #set the auto to scrap
-pos=1
+pos=4
 marca = list_marca[pos]
 modello = list_modello[pos]
 csv_file_name = list_file_out_name[pos]
-pdb.set_trace()
 ##################
 #run the scraper
 driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
@@ -125,20 +130,22 @@ disabled_next = get_disabled_bool(soup)
 
 #loop on the pages until disabled_next=True
 while not disabled_next:
-    #wait to avoid to load the web site too much
-    time.sleep(5)
-    #check that the button 'next-page' is ready
-    element = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, "//li[@class='next-page']")))
-    element.click()
+    #click on 'next-page' button
+    driver.find_element(By.XPATH, "//li[@class='next-page']").click()
+    #wait
+    time.sleep(3)
+    #check the page is loaded
+    WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, "//div[@class='cl-list-element cl-list-element-gap']")))
     #parse the page
     soup = BeautifulSoup(driver.page_source, features="lxml")
     #parse the auto's list
     list_autos = soup.find_all('div', class_="cl-list-element cl-list-element-gap")
     #get the data
     df_as24 = get_info_auto(list_autos, df_as24)
+    print(df_as24.tail())
     #update the boolean
     disabled_next = get_disabled_bool(soup)
-    pdb.set_trace()
+#    pdb.set_trace()
 #exit firefox
 driver.quit()
 #write a csv file
